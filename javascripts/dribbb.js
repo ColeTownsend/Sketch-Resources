69d3e0eb1f3c9f688d2bf7a1c3746076a77e5f87
ColeTownsend <cole@coletownsend.com>
2013-06-23 00:45:01 -0400

    scripts

--- javascripts/dribbb.js
+++ javascripts/dribbb.js
@@ -1,13 +1,11 @@
-$(document).ready(function () {		
-	$.jribbble.getShotsByPlayerId('coletownsend', function (playerShots) {
-	    var html = [];
-	
-	    $.each(playerShots.shots, function (i, shot) {
-	        html.push('<a href="' + shot.url + '" target="_blank">');
-	        html.push('<img src="' + shot.image_url + '" ');
-	        html.push('alt="' + shot.title + '"></a>');
-	    });
-	
-	    $('#drib').html(html.join(''));
-	}, {page: 1, per_page: 12});
-});
+var callback = function (shot) {
+            var html = '';
+
+            html += '<a href="' + shot.url + '" target="_blank">';
+	        html += '<img src="' + shot.image_url + '" ';
+	        html += 'alt="' + shot.title + '"></a>';
+
+            document.getElementById('#drib').innerHTML = html;
+        };
+
+        jribbble.getShotById('914651', callback);
